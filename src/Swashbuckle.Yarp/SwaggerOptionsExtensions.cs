c49ebba6710e81e7332b58f3db5ee46348cd07da
ctyar <ctyar@outlook.com>
2025-11-14 14:20:06 +0000

    Cleanp up the code a bit

--- src/Swashbuckle.Yarp/SwaggerOptionsExtensions.cs
+++ src/Swashbuckle.Yarp/SwaggerOptionsExtensions.cs
@@ -21,7 +21,7 @@ public static class SwaggerOptionsExtensions
     {
         options.PreSerializeFilters.Add((document, request) =>
         {
-            var isForwarded = request.Headers["X-Forwarded-Host"].Any();
+            var isForwarded = request.Headers["X-Forwarded-Host"].Count != 0;
 
             if (!isForwarded)
             {
